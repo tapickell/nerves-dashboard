b2a21f8fff508e8889198626bf3febecf266210b
Todd Pickell <tapickell@gmail.com>
2019-03-30 10:20:14 -0500

    added simple guages and does not break without sensors existing yet :)

--- dashboard/lib/scenes/gpio_info.ex
+++ dashboard/lib/scenes/gpio_info.ex
@@ -9,20 +9,20 @@ defmodule Dashboard.Scene.GpioInfo do
 
   @graph Graph.build(font_size: 22, font: :roboto_mono)
   |> sys_info("GpioInfo")
-  # |> group(
-  # fn g ->
-  #   g
-  #   |> simple_gauge("Ambient Temperature", sensor: :gpio_air_temperature, postfix: "°f")
-  # end,
-  # t: {280, 30}
-  # )
-  # |> group(
-  # fn g ->
-  #   g
-  #   |> simple_gauge("Humidity Level", sensor: :gpio_humidity, postfix: "%")
-  # end,
-  # t: {340, 240}
-  # )
+  |> group(
+  fn g ->
+    g
+    |> simple_gauge("Ambient Temperature", sensor: :gpio_air_temperature, postfix: "°f")
+  end,
+  t: {280, 30}
+  )
+  |> group(
+  fn g ->
+    g
+    |> simple_gauge("Humidity Level", sensor: :gpio_humidity, postfix: "%")
+  end,
+  t: {340, 240}
+  )
 
   def init(_, opts) do
     {:ok, %{graph: @graph}, push: @graph}
