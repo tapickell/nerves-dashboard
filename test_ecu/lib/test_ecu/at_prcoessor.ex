1e1c0479dc4d62098d6e9f366817e96917b31de9
Todd Pickell <tapickell@gmail.com>
2019-04-12 16:00:55 -0500

    return ok and command back

--- test_ecu/lib/test_ecu/at_prcoessor.ex
+++ test_ecu/lib/test_ecu/at_prcoessor.ex
@@ -28,35 +28,36 @@ defmodule TestEcu.AtProcessor do
     with {:ok, command_post} <- command_post(command),
          {:ok, response} <- process_command(command_post) do
       Loggger.info("Command #{command} processed")
+      {:ok, response}
     end
   end
 
 
   defp process_command(command) do
     case command do
-      <<@d, _::binary>> -> AtCommands.atd()
-      <<@z, _::binary>> -> AtCommands.atz()
-      <<@i, _::binary>> -> AtCommands.ati()
-      <<@e, _::binary>> -> AtCommands.atex(command)
-      <<@l, _::binary>> -> AtCommands.atlx(command)
-      <<@m, _::binary>> -> AtCommands.atmx(command)
-      <<@s, @p, _::binary>> -> AtCommands.atspx(command)
-      <<@s, _::binary>> -> AtCommands.atsx(command)
-      <<@h, _::binary>> -> AtCommands.athx(command)
-      <<@a, @t, _::binary>> -> AtCommands.atatx(command)
-      <<@d, @p, _::binary>> -> AtCommands.atdpn(command)
-      <<@d, @e, @s, _::binary>> -> AtCommands.atdesc(command)
-      <<@at, @one, _::binary>> -> AtCommands.atdesc(command)
-      <<@p, @c, _::binary>> -> AtCommands.atpc(command)
-      _ -> {:error, "Unable to process command: #{command}"}
+      <<@d, _::binary>> -> {AtCommands.atd(), command}
+      <<@z, _::binary>> -> {AtCommands.atz(), command}
+      <<@i, _::binary>> -> {AtCommands.ati(), command}
+      <<@e, _::binary>> -> {AtCommands.atex(command), command}
+      <<@l, _::binary>> -> {AtCommands.atlx(command), command}
+      <<@m, _::binary>> -> {AtCommands.atmx(command), command}
+      <<@s, @p, _::binary>> -> {AtCommands.atspx(command), command}
+      <<@s, _::binary>> -> {AtCommands.atsx(command), command}
+      <<@h, _::binary>> -> {AtCommands.athx(command), command}
+      <<@a, @t, _::binary>> -> {AtCommands.atatx(command), command}
+      <<@d, @p, _::binary>> -> {AtCommands.atdpn(command), command}
+      <<@d, @e, @s, _::binary>> -> {AtCommands.atdesc(command), command}
+      <<@at, @one, _::binary>> -> {AtCommands.atdesc(command), command}
+      <<@p, @c, _::binary>> -> {AtCommands.atpc(command), command}
+      _ -> error_command
     end
   end
 
   defp command_post(<<@a, @t, @spc, post::binary>>), do: {:ok, post}
   defp command_post(<<@a, @t, post::binary>>), do: {:ok, post}
+  defp command_post(command), do: error(command)
 
-  defp command_post(command) do
-    {:error, "Unable to process command: #{command}"}
+  defp error(command) do
+    {:error, command, "Unable to process command."}
   end
-
 end
