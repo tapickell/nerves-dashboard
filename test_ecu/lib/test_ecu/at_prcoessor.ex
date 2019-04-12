35200b1193a7d29f1e421e1cdbed1813506571a6
tpickle <tapickell@gmail.com>
2019-04-12 16:37:08 -0500

    lets favor exact command matches

--- test_ecu/lib/test_ecu/at_prcoessor.ex
+++ test_ecu/lib/test_ecu/at_prcoessor.ex
@@ -15,49 +15,54 @@ defmodule TestEcu.AtProcessor do
   @i TestEcu.Hex.hx("I")
   @l TestEcu.Hex.hx("L")
   @m TestEcu.Hex.hx("M")
+  @n TestEcu.Hex.hx("N")
   @one TestEcu.Hex.hx("1")
   @p TestEcu.Hex.hx("P")
   @s TestEcu.Hex.hx("S")
   @spc TestEcu.Hex.hx(" ")
   @t TestEcu.Hex.hx("T")
+  @x TestEcu.Hex.hx("X")
   @z TestEcu.Hex.hx("Z")
 
   def process(command) do
     Logger.info("RX AT: #{command}")
 
-    with {:ok, command_post} <- command_post(command),
-         {:ok, response} <- process_command(command_post) do
-      Loggger.info("Command #{command} processed")
+    with {:ok, response} <- process_command(command) do
+      Logger.info("Command #{command} processed")
       {:ok, response}
     end
   end
 
 
   defp process_command(command) do
-    case command do
-      <<@d, _::binary>> -> {AtCommands.atd(), command}
-      <<@z, _::binary>> -> {AtCommands.atz(), command}
-      <<@i, _::binary>> -> {AtCommands.ati(), command}
-      <<@e, _::binary>> -> {AtCommands.atex(command), command}
-      <<@l, _::binary>> -> {AtCommands.atlx(command), command}
-      <<@m, _::binary>> -> {AtCommands.atmx(command), command}
-      <<@s, @p, _::binary>> -> {AtCommands.atspx(command), command}
-      <<@s, _::binary>> -> {AtCommands.atsx(command), command}
-      <<@h, _::binary>> -> {AtCommands.athx(command), command}
-      <<@a, @t, _::binary>> -> {AtCommands.atatx(command), command}
-      <<@d, @p, _::binary>> -> {AtCommands.atdpn(command), command}
-      <<@d, @e, @s, _::binary>> -> {AtCommands.atdesc(command), command}
-      <<@at, @one, _::binary>> -> {AtCommands.atdesc(command), command}
-      <<@p, @c, _::binary>> -> {AtCommands.atpc(command), command}
-      _ -> error_command
+    case  command_post(command) do
+      {:ok, post} ->
+        case post do
+          <<@d>> -> {AtCommands.atd(), command}
+          <<@z>> -> {AtCommands.atz(), command}
+          <<@i>> -> {AtCommands.ati(), command}
+          <<@e, @x>> -> {AtCommands.atex(command), command}
+          <<@l, @x>> -> {AtCommands.atlx(command), command}
+          <<@m, @x>> -> {AtCommands.atmx(command), command}
+          <<@s, @p, @x>> -> {AtCommands.atspx(command), command}
+          <<@s, @x>> -> {AtCommands.atsx(command), command}
+          <<@h, @x>> -> {AtCommands.athx(command), command}
+          <<@a, @t, @x>> -> {AtCommands.atatx(command), command}
+          <<@d, @p, @n>> -> {AtCommands.atdpn(command), command}
+          <<@d, @e, @s, @c>> -> {AtCommands.atdesc(command), command}
+          <<@at, @one>> -> {AtCommands.atdesc(command), command}
+          <<@p, @c>> -> {AtCommands.atpc(command), command}
+          _ -> command_error(command)
+        end
+      {:error, command, error} -> {:error, command, error}
     end
   end
 
   defp command_post(<<@a, @t, @spc, post::binary>>), do: {:ok, post}
   defp command_post(<<@a, @t, post::binary>>), do: {:ok, post}
-  defp command_post(command), do: error(command)
+  defp command_post(command), do: command_error(command)
 
-  defp error(command) do
+  defp command_error(command) do
     {:error, command, "Unable to process command."}
   end
 end
